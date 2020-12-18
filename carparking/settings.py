217660cea6819cc7d04d8b116f7818d97d751119
pulkit0910 <40095010+pulkit0910@users.noreply.github.com>
2020-12-18 22:45:46 +0530

    Update settings.py

--- carparking/settings.py
+++ carparking/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'car'
+    'car',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
